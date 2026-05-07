e1096b266215337e69ce5799ec1d3dee374754ac
Oscar <oscaraaron.pv@gmail.com>
2026-05-07 18:45:13 +0200

    Feat: Crear ArticuloViewModel y añadir RecyclerView a activity_main

---
+++ app/src/main/java/com/oscar/mintory/ui/ArticuloViewModel.java
@@ -0,0 +1,28 @@
+package com.oscar.mintory.ui;
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import com.oscar.mintory.data.ArticuloRepository;
+import com.oscar.mintory.model.Articulo;
+import java.util.List;
+
+public class ArticuloViewModel extends AndroidViewModel {
+
+    private ArticuloRepository repository;
+    private LiveData<List<Articulo>> todosLosArticulos;
+
+    public ArticuloViewModel(Application application) {
+        super(application);
+        repository = new ArticuloRepository(application);
+        todosLosArticulos = repository.getTodosLosArticulos();
+    }
+
+    public LiveData<List<Articulo>> getTodosLosArticulos() {
+        return todosLosArticulos;
+    }
+
+    public void insertar(Articulo articulo) {
+        repository.insertar(articulo);
+    }
+}
